c16bea5ff4549c029c1ba47522da714632da3521
Your Name <you@example.com>
2025-11-26 13:25:22 +0530

    Replace orange navbar with dark gradient theme

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,41 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="bg-orange-500 text-white px-6 py-4 flex justify-between items-center shadow-md">
+    <nav className="navbar">
       {/* Logo */}
-      <Link
-        to="/"
-        className="text-2xl font-bold hover:text-yellow-300 transition"
-      >
-        MyShop
+      <Link to="/" className="navbar-logo">
+        ✨ MyShop
       </Link>
 
       {/* Menu Links */}
-      <div className="space-x-6">
-        <Link
-          to="/"
-          className="hover:text-yellow-300 relative after:block after:h-[2px] after:bg-yellow-300 after:scale-x-0 hover:after:scale-x-100 after:transition after:origin-left"
-        >
+      <div className="navbar-menu">
+        <Link to="/" className="navbar-link">
           Home
         </Link>
-        <Link
-          to="/products"
-          className="hover:text-yellow-300 relative after:block after:h-[2px] after:bg-yellow-300 after:scale-x-0 hover:after:scale-x-100 after:transition after:origin-left"
-        >
+        <Link to="/products" className="navbar-link">
           Products
         </Link>
-        <Link
-          to="/cart"
-          className="hover:text-yellow-300 relative after:block after:h-[2px] after:bg-yellow-300 after:scale-x-0 hover:after:scale-x-100 after:transition after:origin-left"
-        >
+        <Link to="/cart" className="navbar-link">
           Cart
         </Link>
-        <Link
-          to="/orders"
-          className="hover:text-yellow-300 relative after:block after:h-[2px] after:bg-yellow-300 after:scale-x-0 hover:after:scale-x-100 after:transition after:origin-left"
-        >
+        <Link to="/orders" className="navbar-link">
           Orders
         </Link>
       </div>
